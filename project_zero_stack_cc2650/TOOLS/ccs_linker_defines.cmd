d7bf93d23fd9771c87c12fdd26d1c58d9939072c
rco123 <sgkim123456@daum.net>
2017-06-06 00:38:04 +0900

    init

---
+++ project_zero_stack_cc2650/TOOLS/ccs_linker_defines.cmd
@@ -0,0 +1,10 @@
+/*
+** Stack Frontier Generator 1.1.0 (2017-05-30 15:50:11.125000)
+**
+** WARNING - Auto-generated file. Modifications could be lost!
+*/
+
+--define=ICALL_RAM0_START=0x20004348
+--define=ICALL_STACK0_START=0xf000
+--define=ICALL_STACK0_ADDR=0xf001
+
